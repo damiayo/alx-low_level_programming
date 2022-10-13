c05bd581db9403e0090e4fea7c0e7f8684667e6d
damiayo <dahmmy5776@gmail.com>
2022-10-13 13:16:07 -0700

    This is a fact

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,3 @@
+#include <studio.h>
+/** * main - Prints "Programming is like building a multilingual * puzzle
+ *  *Return: Always 0. */int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);}
