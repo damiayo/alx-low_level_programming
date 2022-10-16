b5fa3a29c7c74d461a0ec72ede5d7bf16f403eb8
damiayo <dahmmy5776@gmail.com>
2022-10-16 14:56:34 -0700

    It is a wrap

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry
  * Return: 1
  */
-int main
+int main(void)
 {
-	Write (1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	Write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
