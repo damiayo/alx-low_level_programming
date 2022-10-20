a54cb23544d539f1a2ae9527f63e7f9d9edaa1fd
damiayo <dahmmy5776@gmail.com>
2022-10-20 15:38:13 -0700

    Wert

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,20 @@
  * Return: 0
  */
 
-int main(void)
+main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n));
+	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i > 10)
-	 * {
-	 *	putchar(i);
-	 * }
-	 */
-	printf("infinite loop avoided : \\o / \n");
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
